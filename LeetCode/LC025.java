ff806ff5380e8b88d2f477e74abe1b93aeddf4fa
FrankZh27 <jzho@umich.edu>
2018-08-25 16:04:50 -0700

    update

--- LeetCode/LC025.java
+++ LeetCode/LC025.java
@@ -32,3 +32,41 @@ class Solution {
         }
     }
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        ListNode curr = head;
+        int count = k;
+        while (curr != null && count-- > 0) {
+            curr = curr.next;
+        }
+        if (curr == null && count > 0) {
+            return head;
+        }
+        if (curr == null) {
+            count--;
+        }
+        curr = reverseKGroup(curr, k);
+        ListNode tempNode = head;
+        while (tempNode != null && count++ < k-2) {
+            tempNode = tempNode.next;
+            head.next = curr;
+            curr = head;
+            head = tempNode;
+        }
+        tempNode.next = curr;
+        return tempNode;
+    }
+}
